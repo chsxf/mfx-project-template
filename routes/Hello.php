0fa914e8b75824bf4191d184098b7193ae700947
Christophe SAUVEUR <chsxf.pro@gmail.com>
2024-08-03 23:35:53 +0200

    ⬆️ Support for MFX 2.0

--- routes/Hello.php
+++ routes/Hello.php
@@ -1,12 +1,13 @@
 <?php
 
+use chsxf\MFX\Attributes\AnonymousRoute;
 use chsxf\MFX\Attributes\Route;
 use chsxf\MFX\RequestResult;
 use chsxf\MFX\Routers\IRouteProvider;
 
 class Hello implements IRouteProvider
 {
-    #[Route]
+    #[Route, AnonymousRoute]
     public static function world()
     {
         return new RequestResult(null, ['php_version' => PHP_VERSION]);
